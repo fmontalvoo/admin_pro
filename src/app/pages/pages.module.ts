67b097dd5f52c6c4fc6cd60c416bd653e38d15bb
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-21 15:19:26 -0500

    Se modifica el porcentaje de progreso en la barra.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 // import { PagesRoutingModule } from './pages-routing.module';
@@ -14,6 +15,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
     SharedModule,
     // PagesRoutingModule,
   ],
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  public percent: number = 50;
 
-  ngOnInit(): void {
+  get progress(): string {
+    return `${this.percent}%`;
+  }
+
+  public changePercent(value: number): void {
+    if (this.percent <= 0 && value < 0) {
+      this.percent = 0;
+    } else if (this.percent >= 100 && value >= 0) {
+      this.percent = 100;
+    } else {
+      this.percent += value;
+    }
   }
 
 }
