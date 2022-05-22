f4934dfaa7f50c0831b2d5cbe14090e4dbd977fd
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-22 14:08:23 -0500

    Se hace la navegacion a la pagina de account services.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -8,6 +9,7 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
   ],
   declarations: [
     HeaderComponent,
