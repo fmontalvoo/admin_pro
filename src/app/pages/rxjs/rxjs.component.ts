b4c837ec6fdb51f1862122f2416eee1c9ecbcda8
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-22 17:15:32 -0500

    Se define un observable en el componente RXJS.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
@@ -11,6 +13,33 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const obsersable = new Observable(observer => {
+      let i = -1;
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i === 20) {
+          clearInterval(interval);
+          observer.complete();
+        }
+        if (i === 21) {
+          clearInterval(interval);
+          observer.error('Error');
+        }
+      }, 1000);
+    });
+
+    // obsersable.subscribe(
+    //   value => console.log(value),
+    //   error => console.error(error),
+    //   () => console.info('Completado')
+    // );
+
+    obsersable.subscribe({
+      next: value => console.log(value),
+      error: error => console.error(error),
+      complete: () => console.info('Completado'),
+    });
   }
 
 }
