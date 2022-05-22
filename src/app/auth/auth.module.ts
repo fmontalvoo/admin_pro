bea08b01dcd781437bf9d56609b8330e6801cf8d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-22 14:44:17 -0500

    Invocacion de script js desde componente de angular.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -9,6 +10,7 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     AuthRoutingModule,
   ],
   declarations: [
