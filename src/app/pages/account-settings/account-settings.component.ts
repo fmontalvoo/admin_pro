cd7d204ad8be770d66d33809bbfaf9cb816d8496
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-22 13:42:51 -0500

    Se implementa funcionalidad para identificar el tema selecionado actualmente.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,18 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   private ref: Element | null;
+  private links!: NodeListOf<Element> | null;
 
   constructor() {
     this.ref = document.querySelector('#theme');
   }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
-  public changeColorTheme(theme: string) {
+  public changeColorTheme(theme: string): void {
     const stylePath = `./assets/css/colors/${theme}.css`;
     this.ref?.setAttribute('href', stylePath);
     localStorage.setItem('theme', stylePath);
+    this.checkCurrentTheme();
   }
+
+  public checkCurrentTheme() {
+    this.links?.forEach(link => {
+      link.classList.remove('working');
+      const theme = link.getAttribute('data-theme');
+      const stylePath = `./assets/css/colors/${theme}.css`;
+      const currentStyle = this.ref?.getAttribute('href');
+      if (currentStyle === stylePath) {
+        link.classList.add('working');
+      }
+    });
+  }
+
 
 }
