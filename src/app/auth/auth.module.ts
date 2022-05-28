7abc6e63e29fa45360a565677eb0be82bf7ca1c5
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-28 15:33:55 -0500

    Se valida el formulario de registro de usuario.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -11,6 +12,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     AuthRoutingModule,
   ],
   declarations: [
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(3)]],
+      terms: [false, [Validators.required]]
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.registerForm.invalid) {
+      return Object.values(this.registerForm.controls)
+        .forEach(control => {
+          if (control instanceof FormGroup)
+            Object.values(control.controls).forEach(ctrl => ctrl.markAsTouched());
+          else
+            control.markAsTouched();
+        });
+    }
+  }
+
+  public isInValid(input: string) {
+    return this.registerForm.get(input)?.invalid && this.registerForm.get(input)?.touched;
+  }
+
+  public isValid(input: string) {
+    return this.registerForm.get(input)?.valid;
+  }
+
+  public termsAcepted(): boolean {
+    return this.registerForm.get('terms')?.value;
   }
 
 }
